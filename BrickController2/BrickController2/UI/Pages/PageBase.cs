baf478923d2ca54db64a538a45aa0d1e1c42541a
Istvan Murvai <imurvai@gmail.com>
2018-08-07 23:36:16 +0200

    Added ActionSheet and Alert viewmodel support.

--- BrickController2/BrickController2/UI/Pages/PageBase.cs
+++ BrickController2/BrickController2/UI/Pages/PageBase.cs
@@ -5,22 +5,51 @@ namespace BrickController2.UI.Pages
 {
     public abstract class PageBase : ContentPage
     {
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            SetViewModelDisplayCallbacks();
+        }
+
         protected override void OnAppearing()
         {
             (BindingContext as ViewModelBase)?.OnAppearing();
             base.OnAppearing();
+            SetViewModelDisplayCallbacks();
         }
 
         protected override void OnDisappearing()
         {
             (BindingContext as ViewModelBase)?.OnDisappearing();
             base.OnDisappearing();
+            ClearViewModelDisplayCallbacks();
         }
 
         protected override bool OnBackButtonPressed()
         {
             (BindingContext as ViewModelBase)?.OnBackButtonPressed();
             return base.OnBackButtonPressed();
+        }
+
+        private void SetViewModelDisplayCallbacks()
+        {
+            if (BindingContext is ViewModelBase vm)
+            {
+                vm.OnDisplayAlert = (title, message, accept, cancel) => string.IsNullOrEmpty(accept) ?
+                    DisplayAlert(title, message, cancel) :
+                    DisplayAlert(title, message, accept, cancel);
+
+                vm.OnDisplayActionSheet = (title, cancel, destruction, buttons) => DisplayActionSheet(title, cancel, destruction, buttons);
+            }
+        }
+
+        private void ClearViewModelDisplayCallbacks()
+        {
+            if (BindingContext is ViewModelBase vm)
+            {
+                vm.OnDisplayAlert = null;
+                vm.OnDisplayActionSheet = null;
+            }
         }
     }
 }
--- BrickController2/BrickController2/UI/ViewModels/ViewModelBase.cs
+++ BrickController2/BrickController2/UI/ViewModels/ViewModelBase.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using BrickController2.UI.Navigation;
 
 namespace BrickController2.UI.ViewModels
@@ -13,9 +15,40 @@ namespace BrickController2.UI.ViewModels
 
         protected INavigationService NavigationService { get; }
 
+        #region View lifecycle callbacks
+
         public virtual void OnAppearing() { }
         public virtual void OnDisappearing() { }
         public virtual void OnBackButtonPressed() { }
+
+        #endregion
+
+        #region DisplayAlert
+
+        public Func<string, string, string, string, Task> OnDisplayAlert { get; set; }
+
+        protected Task DisplayAlert(string title, string message, string cancel)
+        {
+            return OnDisplayAlert?.Invoke(title, message, null, cancel);
+        }
+
+        protected Task DisplayAlert(string title, string message, string accept, string cancel)
+        {
+            return OnDisplayAlert?.Invoke(title, message, accept, cancel);
+        }
+
+        #endregion
+
+        #region DisplayActionSheet
+
+        public Func<string, string, string, string[], Task<string>> OnDisplayActionSheet { get; set; }
+
+        protected Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons)
+        {
+            return OnDisplayActionSheet?.Invoke(title, cancel, destruction, buttons);
+        }
+
+        #endregion
 
         #region INotifyPropertyChanged
 
