155a52231f094a6afada579404b61a9cbfb5864c
imurvai <gienter1>
2018-09-10 09:28:19 +0200

    Added the RadioButtonGroup control (not finished).

---
+++ BrickController2/BrickController2/UI/Controls/RadioButtonGroup.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BrickController2.UI.Controls
+{
+    public class RadioButtonGroup : ContentView
+    {
+
+        public string ItemsCsv
+        {
+            get;
+            set;
+        }
+
+        public StackOrientation Orientation
+        {
+            get;
+            set;
+        }
+
+        public int SelectedIndex
+        {
+            get;
+            set;
+        }
+
+        public ICommand SelectionChangedCommand
+        {
+            get;
+            set;
+        }
+    }
+}
