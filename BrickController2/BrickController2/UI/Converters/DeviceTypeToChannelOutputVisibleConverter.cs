58c9cd7488ea68ad9ab7f2fa5ed2fc81a676730e
Istvan Murvai <imurvai@gmail.com>
2018-09-15 23:33:46 +0200

    UI update.

---
+++ BrickController2/BrickController2/UI/Converters/DeviceTypeToChannelOutputVisibleConverter.cs
@@ -0,0 +1,29 @@
+﻿using BrickController2.DeviceManagement;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BrickController2.UI.Converters
+{
+    public class DeviceTypeToChannelOutputVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var deviceType = (DeviceType)value;
+            switch (deviceType)
+            {
+                case DeviceType.Boost:
+                case DeviceType.PoweredUp:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
