a65e359d85c10f9e17f5693bb0bdb857b026a7b9
vicocz <vico@centrum.cz>
2019-12-02 22:07:50 +0100

    Device test page - Show channel names

---
+++ BrickController2/BrickController2/Helpers/ObservableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BrickController2.Helpers
+{
+    public static class ObservableExtensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            return new ObservableCollection<T>(source);
+        }
+    }
+}
---
+++ BrickController2/BrickController2/UI/ViewModels/DeviceChannelOutputViewModel.cs
@@ -0,0 +1,43 @@
+﻿using BrickController2.DeviceManagement;
+using BrickController2.Helpers;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Device = BrickController2.DeviceManagement.Device;
+
+namespace BrickController2.UI.ViewModels
+{
+    // Copied out from BrickController2.UI.Controls.DeviceOutputTesterControl.DeviceOutputViewModel
+    public class DeviceChannelOutputViewModel : NotifyPropertyChangedSource
+    {
+        private int _output;
+
+        public DeviceChannelOutputViewModel(Device device, int channel)
+        {
+            Device = device;
+            Channel = channel;
+            Output = 0;
+
+            TouchUpCommand = new Command(() => Output = 0);
+        }
+
+        public Device Device { get; }
+        public DeviceType DeviceType => Device.DeviceType;
+        public int Channel { get; }
+
+        public int MinValue => -100;
+        public int MaxValue => 100;
+
+        public int Output
+        {
+            get { return _output; }
+            set
+            {
+                _output = value;
+                Device.SetOutput(Channel, (float)value / MaxValue);
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand TouchUpCommand { get; }
+    }
+}
