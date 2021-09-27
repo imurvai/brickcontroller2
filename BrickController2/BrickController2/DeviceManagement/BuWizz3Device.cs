d73e38495b43650dcd0daedd0ed51af58886104d
Streve <imurvai@gmail.com>
2021-09-27 20:05:32 +0200

    BW3 basic usage

--- BrickController2/BrickController2/DeviceManagement/BuWizz3Device.cs
+++ BrickController2/BrickController2/DeviceManagement/BuWizz3Device.cs
@@ -1,4 +1,5 @@
-﻿using BrickController2.PlatformServices.BluetoothLE;
+﻿using BrickController2.Helpers;
+using BrickController2.PlatformServices.BluetoothLE;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,7 +45,7 @@ namespace BrickController2.DeviceManagement
             CheckChannel(channel);
             value = CutOutputValue(value);
 
-            var intValue = (int)(value * 255);
+            var intValue = (int)(value * 127);
 
             lock (_outputLock)
             {
@@ -70,8 +71,122 @@ namespace BrickController2.DeviceManagement
             return Task.FromResult(_characteristic != null && _firmwareRevisionCharacteristic != null && _modelNumberCharacteristic != null);
         }
 
+        protected override async Task<bool> AfterConnectSetupAsync(bool requestDeviceInformation, CancellationToken token)
+        {
+            try
+            {
+                if (requestDeviceInformation)
+                {
+                    await ReadDeviceInfo(token);
+                }
+            }
+            catch { }
+
+            return true;
+        }
+
         protected override async Task ProcessOutputsAsync(CancellationToken token)
         {
+            try
+            {
+                lock (_outputLock)
+                {
+                    _outputValues[0] = 0;
+                    _outputValues[1] = 0;
+                    _outputValues[2] = 0;
+                    _outputValues[3] = 0;
+                    _outputValues[4] = 0;
+                    _outputValues[5] = 0;
+                    _lastOutputValues[0] = 1;
+                    _lastOutputValues[1] = 1;
+                    _lastOutputValues[2] = 1;
+                    _lastOutputValues[3] = 1;
+                    _lastOutputValues[4] = 1;
+                    _lastOutputValues[5] = 1;
+                    _sendAttemptsLeft = MAX_SEND_ATTEMPTS;
+                }
+
+                while (!token.IsCancellationRequested)
+                {
+                    int v0, v1, v2, v3, v4, v5, sendAttemptsLeft;
+
+                    lock (_outputLock)
+                    {
+                        v0 = _outputValues[0];
+                        v1 = _outputValues[1];
+                        v2 = _outputValues[2];
+                        v3 = _outputValues[3];
+                        v4 = _outputValues[4];
+                        v5 = _outputValues[5];
+                        sendAttemptsLeft = _sendAttemptsLeft;
+                        _sendAttemptsLeft = sendAttemptsLeft > 0 ? sendAttemptsLeft - 1 : 0;
+                    }
+
+                    if (v0 != _lastOutputValues[0] ||
+                        v1 != _lastOutputValues[1] ||
+                        v2 != _lastOutputValues[2] ||
+                        v3 != _lastOutputValues[3] ||
+                        v4 != _lastOutputValues[4] ||
+                        v5 != _lastOutputValues[5] ||
+                        sendAttemptsLeft > 0)
+                    {
+                        if (await SendOutputValuesAsync(v0, v1, v2, v3, v4, v5, token).ConfigureAwait(false))
+                        {
+                            _lastOutputValues[0] = v0;
+                            _lastOutputValues[1] = v1;
+                            _lastOutputValues[2] = v2;
+                            _lastOutputValues[3] = v3;
+                            _lastOutputValues[4] = v4;
+                            _lastOutputValues[5] = v5;
+                        }
+                    }
+                    else
+                    {
+                        await Task.Delay(10, token).ConfigureAwait(false);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private async Task<bool> SendOutputValuesAsync(int v0, int v1, int v2, int v3, int v4, int v5, CancellationToken token)
+        {
+            try
+            {
+                var sendOutputBuffer = new byte[] { 0x30, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+                sendOutputBuffer[1] = (byte)v0;
+                sendOutputBuffer[2] = (byte)v1;
+                sendOutputBuffer[3] = (byte)v2;
+                sendOutputBuffer[4] = (byte)v3;
+                sendOutputBuffer[5] = (byte)v4;
+                sendOutputBuffer[6] = (byte)v5;
+
+                return await _bleDevice?.WriteAsync(_characteristic, sendOutputBuffer, token);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private async Task ReadDeviceInfo(CancellationToken token)
+        {
+            var firmwareData = await _bleDevice?.ReadAsync(_firmwareRevisionCharacteristic, token);
+            var firmwareVersion = firmwareData?.ToAsciiStringSafe();
+            if (!string.IsNullOrEmpty(firmwareVersion))
+            {
+                FirmwareVersion = firmwareVersion;
+            }
+
+            var modelNumberData = await _bleDevice?.ReadAsync(_modelNumberCharacteristic, token);
+            var modelNumber = modelNumberData?.ToAsciiStringSafe();
+            if (!string.IsNullOrEmpty(modelNumber))
+            {
+                HardwareVersion = modelNumber;
+            }
         }
     }
 }
