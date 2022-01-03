abdc94118d94dde34fd69fbda32877f2d81b8b44
Vit Nemecky <vit.nemecky@centrum.cz>
2022-01-03 19:12:26 +0100

    Change PF channel names of Buwizz3 (#77)
    Co-authored-by: Vit Nemecky <vico@centrum.cz>

--- BrickController2/BrickController2/UI/Controls/DeviceChannelLabel.cs
+++ BrickController2/BrickController2/UI/Controls/DeviceChannelLabel.cs
@@ -9,6 +9,7 @@ namespace BrickController2.UI.Controls
     {
         private readonly static string[] _controlPlusChannelLetters = new[] { "A", "B", "C", "D" };
         private readonly static string[] _circuitCubesChannelLetters = new[] { "A", "B", "C" };
+        private readonly static string[] _buwizz3ChannelLetters = new[] { "1", "2", "3", "4", "A", "B" };
 
         public static BindableProperty DeviceTypeProperty = BindableProperty.Create(nameof(DeviceType), typeof(DeviceType), typeof(DeviceChannelLabel), default(DeviceType), BindingMode.OneWay, null, OnDeviceChanged);
         public static BindableProperty ChannelProperty = BindableProperty.Create(nameof(Channel), typeof(int), typeof(DeviceChannelLabel), 0, BindingMode.OneWay, null, OnChannelChanged);
@@ -54,6 +55,10 @@ namespace BrickController2.UI.Controls
 
                 case DeviceType.CircuitCubes:
                     Text = _circuitCubesChannelLetters[Math.Min(Math.Max(Channel, 0), 2)];
+                    break;
+
+                case DeviceType.BuWizz3:
+                    Text = _buwizz3ChannelLetters[Math.Min(Math.Max(Channel, 0), 6)];
                     break;
 
                 case DeviceType.Infrared:
