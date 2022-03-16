e8efb90c1955969accac8bed2281c2d66202a777
Streve <imurvai@gmail.com>
2022-03-16 21:47:10 +0100

    updated the target api for android

--- BrickController2/BrickController2.Android/Resources/Resource.designer.cs
+++ BrickController2/BrickController2.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace BrickController2.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.1.0.10")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
 	public partial class Resource
 	{
 		
